e9e7ca43b7dcf3a0f685493404da5500420adbca
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2020-09-09 14:54:08 -0700

    Added examples for abstract classes.

---
+++ 10AbstractClasses/Notes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10AbstractClasses
+{
+    public interface IValuable
+    {
+        decimal Value { get; set; }
+    }
+
+    public interface IHardness
+    {
+        double Hardness { get; set; }
+    }
+
+    public abstract class Gem2
+    {
+        public abstract string Color { get; set; }
+    }
+
+    public class LapisLazuli : Gem2, IHardness, IValuable
+    {
+        public decimal Value { get; set; }
+        public double Hardness { get; set; }
+
+        public override string Color { get; set; }
+
+        public LapisLazuli()
+        {
+            Value = 80M;
+            Hardness = 5.5;
+            Color = "Deep, ocean blue";
+        }
+    }
+
+public class Silica : Gem2, IHardness
+{
+    public double Hardness { get; set; }
+
+    public override string Color { get; set; }
+
+    public Silica()
+    {
+        Hardness = 2.5;
+        Color = "Rainbow, reflects light";
+    }
+}
+}
