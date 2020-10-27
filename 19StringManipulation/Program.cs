738b73dfe9dbb782ed28daa6e69e561125f72177
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2020-10-27 12:23:20 -0700

    Added first pass at String Manipulation and Dates and Times topics.

---
+++ 19StringManipulation/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace _19StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------CULTURES-----------------------");
+            Console.Write("Please enter a culture code:");
+            string cultureCode = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(cultureCode))
+                cultureCode = "en-US";
+
+            Console.WriteLine("-------------Creating a Culture Object----------------");
+            CultureInfo myCulture = new CultureInfo(cultureCode);
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now.ToString(myCulture.DateTimeFormat) + " (" + myCulture.Name + ")");
+
+            Console.WriteLine("-----------------Invariant Culture--------------------");
+
+            //Invariant Culture
+            var invariantCulture = CultureInfo.InvariantCulture;
+            //Will output in the MM/dd/yyyy 24-hour clock format
+            Console.WriteLine(now.ToString(invariantCulture.DateTimeFormat));
+
+            Console.WriteLine("-----------------ESCAPE CHARACTERS--------------------");
+
+            string withNewline = "This is on the first line.\nThis is on the second.";
+            Console.WriteLine(withNewline);
+
+            string dialogue = "She said, \"I didn't know that was him!\".";
+            Console.WriteLine(dialogue);
+
+
+            Console.WriteLine("---------------------OPERATORS------------------------");
+
+            Console.WriteLine("--------------Interpolated Strings ($)----------------");
+
+            string name1 = "Jack";
+            string name2 = "Quint";
+
+            Console.WriteLine($"Good morning {name1} and {name2}!");
+
+            Console.WriteLine("-----------------Literal Strings (@)------------------");
+            string filePath = @"C:\this\is\a\file\path";
+            Console.WriteLine(filePath);
+
+            Console.WriteLine("---------------------FORMATTING-----------------------");
+
+            decimal money = 5.67M;
+            Console.WriteLine(money.ToString("C"));
+
+            double percentage = 0.67;
+            Console.WriteLine(percentage.ToString("P"));
+
+            Console.WriteLine("-------------------Custom Formats---------------------");
+
+            int phoneNumber = 2125559731;
+            string format = "(###) ###-####";
+            Console.WriteLine(phoneNumber.ToString(format));
+
+            Console.WriteLine("-------------------CONCATENATION----------------------");
+
+            Console.WriteLine("----------------Naive Concatenation-------------------");
+
+            int value = 6;
+
+            Console.WriteLine("The value is " + value.ToString());
+
+            Console.WriteLine("------------------String.Concat()---------------------");
+
+            string hello = "Hello ";
+            string firstName1 = "Jack, ";
+            string firstName2 = "Quint, ";
+            string firstName3 = "June, ";
+            string firstName4 = "and Dirk!";
+
+            var combined = string.Concat(hello, firstName1, firstName2, firstName3, firstName4);
+            Console.WriteLine(combined);
+
+            Console.WriteLine("-------------------StringBuilder---------------------");
+            StringBuilder builder = new StringBuilder();
+
+            for(int i = 0; i < 100; i++)
+            {
+                builder.Append(i);
+                builder.Append(" ");
+            }
+
+            Console.WriteLine(builder.ToString());
+
+            Console.WriteLine("---------------SEARCHING IN STRINGS-----------------");
+
+            Console.WriteLine("--------------------Contains()----------------------");
+
+            string sentence = "This is a sentence.";
+
+            Console.WriteLine(sentence.Contains("is a")); //true
+            Console.WriteLine(sentence.Contains("isa")); //false
+
+            Console.WriteLine("--------------------IndexOf()-----------------------");
+
+            int index = sentence.IndexOf("is");
+            Console.WriteLine($"Found the substring 'is' at position {index}");
+
+            Console.WriteLine("-----------StartsWith() and EndsWith()--------------");
+
+            bool startsWith = sentence.StartsWith("This"); //true
+            bool endsWith = sentence.EndsWith("tence"); //false, missing the period
+
+            Console.WriteLine(startsWith);
+            Console.WriteLine(endsWith);
+
+            Console.WriteLine("---------------STRING MANIPULATION------------------");
+
+            Console.WriteLine("---------------------Trim()-------------------------");
+            
+            string sentenceWhitespace = "     This has some whitespace.     ";
+            Console.WriteLine(sentenceWhitespace.TrimStart() + "End of line.");
+            Console.WriteLine(sentenceWhitespace.TrimEnd() + "End of line.");
+            Console.WriteLine(sentenceWhitespace.Trim() + "End of line."); //Trims whitespace at both beginning and end
+
+            Console.WriteLine("---------------------Padding------------------------");
+
+            string sevenDigitPhone = "1234567";
+
+            Console.WriteLine(sevenDigitPhone.PadLeft(10, '0'));
+            Console.WriteLine(sevenDigitPhone.PadRight(10, '0'));
+
+            Console.WriteLine("-------------------Change Case----------------------");
+
+            string mixedCaseString = "ThIs Is A mIxEd cAsE StRiNg.";
+
+            Console.WriteLine(mixedCaseString.ToUpper());
+            Console.WriteLine(mixedCaseString.ToLower());
+
+            Console.WriteLine("-------------------COMPARISON-----------------------");
+
+            string test1 = "This is a semicolon ;";
+            string test2 = "This is a semicolon ;";
+            string test3 = "This is a semicolon ;"; //Actually it's a greek question mark.
+
+            Console.WriteLine($"test1 and test2 are {(test1 == test2 ? "equal" : "NOT equal")}");
+            Console.WriteLine($"test2 and test3 are {(test2 == test3 ? "equal" : "NOT equal")}");
+
+            string firstString = "This is the First String.";
+            string secondString = "This Is The First String.";
+
+            Console.WriteLine(firstString.Equals(secondString)); //False
+            Console.WriteLine(firstString.Equals(secondString, StringComparison.OrdinalIgnoreCase)); //True
+            Console.WriteLine(firstString.Equals(secondString, StringComparison.InvariantCultureIgnoreCase)); //True
+            Console.WriteLine(firstString.Equals(secondString, StringComparison.InvariantCulture)); //False
+
+
+            Console.WriteLine("----------------SPLITTING STRINGS--------------------");
+
+            string toBeSplit = "This is a bunch of words and we will split this sentence.";
+
+            var words = toBeSplit.Split(' ');
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+
+            Console.WriteLine("--------------Splitting by Multiple------------------");
+
+            string toParse = "This:is another\tstrange/sentence.";
+
+            char[] delimiters = { ':', ' ', '\t', '/' };
+
+            words = toParse.Split(delimiters);
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine("--------------Splitting by Strings------------------");
+
+            string toSplit = "This...is our final>>odd sentence.";
+
+            string[] stringDelimiters = { ">>", "..." };
+
+            words = toSplit.Split(stringDelimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ 20DatesAndTimes/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace _20DatesAndTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------DateTime-----------------------------------");
+
+            DateTime myDateTime = new DateTime(); //1 January 0001 0000 Hours (Midnight)
+
+            DateTime fifteenthJune = new DateTime(1215, 6, 15);
+            var eighteenthFebruary = new DateTime(1930, 2, 18);
+            var twelfthApril = new DateTime(1945, 4, 12, 15, 35, 0);
+
+            var now = DateTime.Now; //The date and time now
+            var utcNow = DateTime.UtcNow; //The Universal Coordinated Time (UTC) value for now.
+            var today = DateTime.Today; //Today's date, with a time value of midnight.
+
+            Console.WriteLine("---------------------DateTime Manipulation-----------------------------");
+
+            var date = DateTime.Now;
+            date = date.AddHours(12);
+            date = date.AddMonths(1);
+            date = date.AddDays(7);
+            date = date.AddMinutes(-30);
+
+            Console.WriteLine(date);
+
+            Console.WriteLine("--------------------Displaying Dates and Times-------------------------");
+
+            Console.WriteLine(DateTime.Now.ToString());
+
+            Console.WriteLine(DateTime.Now.ToString("dddd, dd MM yyyy h:mm:ss gg"));
+
+            Console.WriteLine("--------------------Shorthand Display Methods--------------------------");
+
+            var display = DateTime.Now.ToShortDateString();
+            Console.WriteLine(display);
+
+            var displayTime = DateTime.Now.ToShortTimeString();
+            Console.WriteLine(displayTime);
+
+            var longDisplay = DateTime.Now.ToLongDateString();
+            Console.WriteLine(longDisplay);
+
+            var longDisplayTime = DateTime.Now.ToLongTimeString();
+            Console.WriteLine(longDisplayTime);
+
+            Console.WriteLine("-------------------------Parsing a String------------------------------");
+
+            var parsingDate = DateTime.Parse("10/24/2020 05:50AM");
+            Console.WriteLine(parsingDate.ToString());
+
+            //Try changing the string below to an unparsable date.
+            DateTime parsedDate;
+            bool isParsable = DateTime.TryParse("5/4/2012 11:30", out parsedDate);
+            Console.WriteLine("Parsable? " + isParsable + ". Value: " + parsedDate.ToString());
+
+            string format = "ddd MM/dd/yyyy h:mm:ss";
+            parsedDate = DateTime.ParseExact("Mon 10/26/2020 9:15:45", format, null);
+            Console.WriteLine(parsedDate);
+
+
+            Console.WriteLine("----------------------------TimeSpan----------------------------------");
+
+            var newTimeSpan = new TimeSpan();
+            Console.WriteLine(newTimeSpan);
+
+            //7 days, 8 hours, 10 minutes, and 35 seconds
+            newTimeSpan = new TimeSpan(7, 8, 10, 35);
+            Console.WriteLine(newTimeSpan); //7.08:10:35
+
+            Console.WriteLine("----------------Math with DateTime and TimeSpan------------------------");
+
+            DateTime startDate = new DateTime(2020, 11, 10, 9, 35, 0);
+            DateTime endDate = new DateTime(2020, 11, 14, 15, 10, 20);
+
+            TimeSpan duration = endDate - startDate;
+            Console.WriteLine(duration); //4.05:35:20
+
+            duration = new TimeSpan(15, 45, 30); //15 hours, 45 minutes, 30 seconds
+
+            DateTime initialDate = DateTime.Now;
+            var newDate = initialDate.Add(duration);
+            Console.WriteLine(newDate);
+
+            newDate = newDate.Subtract(duration);
+            Console.WriteLine(newDate); //Should be now
+
+            TimeSpan negative = new TimeSpan(-4, 30, 12);
+            Console.WriteLine(negative);
+            Console.WriteLine(negative.Duration()); //Positive value
+            Console.WriteLine(negative.Negate()); //Positive value; negating a negative value results in a positive value.
+
+            Console.WriteLine("--------------------------TimeZoneInfo--------------------------------");
+
+            TimeZoneInfo mountainStandard = TimeZoneInfo.FindSystemTimeZoneById("Mountain Standard Time");
+            Console.WriteLine(mountainStandard);
+
+            var allTimeZones = TimeZoneInfo.GetSystemTimeZones();
+            foreach(var timeZone in allTimeZones)
+            {
+                //Uncomment the below line to output all time zones on this machine.
+                //Console.WriteLine(timeZone);
+            }
+        }
+    }
+}
